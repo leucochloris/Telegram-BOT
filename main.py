b1195b9897cd210c8856f240323186a0914a3fd8
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-08-10 17:29:31 +0300

    Just forget make a last backup

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ bot = telebot.TeleBot("5596281432:AAGebrPobQ-RZHe7DnZUFhHRyC6t0rkK198",
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Sup!?")
+    bot.reply_to(message, "Ooh, howdy.... haven't seen you for a long time!")
 
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    if message.text == 'эй':
+        bot.reply_to(message, 'В жёпу себе ЭЙ`кни!')
+    else:
+        bot.reply_to(message, 'I DONT understand you...')
+
+
+    # bot.reply_to(message, message.text)
 
 
 bot.infinity_polling()
