bddbbd9bbc72c37455b5c42681025e5499a33c72
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-08-17 08:19:18 +0300

    Connect SQLite3 to our BOT

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import telebot
+import sqlite3
+
 
 #   5596281432:AAGebrPobQ-RZHe7DnZUFhHRyC6t0rkK198
 
@@ -8,8 +10,30 @@ bot = telebot.TeleBot("5596281432:AAGebrPobQ-RZHe7DnZUFhHRyC6t0rkK198",
 
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Ooh, howdy.... haven't seen you for a long time!")
+def start(message):
+    connect = sqlite3.connect('purple.db')
+    cursor = connect.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS purple_data (
+        id INTEGER
+    )""")
+    connect.commit()
+
+
+    #check data (uniq users should be ...)
+    purple_id = message.chat.id
+    cursor.execute(f"select id from purple_data where id = {purple_id}")
+    data = cursor.fetchone()
+    if data is None:
+        #add values
+        purple_list = [message.chat.id]
+        cursor.execute("INSERT INTO purple_data VALUES(?);", purple_list)
+        connect.commit()
+    else:
+        bot.send_message(message.chat.id, 'Ooh.... you are already here!')
+    # bot.reply_to(message, "Ooh, howdy.... thanks for join us!")
+
+
 
 
 @bot.message_handler(func=lambda m: True)
