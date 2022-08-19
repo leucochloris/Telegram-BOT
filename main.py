f0cce9e3b4871f9dccd97b567faae46c5cacac37
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-08-19 14:03:10 +0300

    Add a some new function (append and delete login in DB)

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import telebot
 import sqlite3
 
-
-#   5596281432:AAGebrPobQ-RZHe7DnZUFhHRyC6t0rkK198
-
-
 bot = telebot.TeleBot("5596281432:AAGebrPobQ-RZHe7DnZUFhHRyC6t0rkK198",
-                      parse_mode=None)  # You can set parse_mode by default. HTML or MARKDOWN
+                      parse_mode=None)
 
-
-@bot.message_handler(commands=['start', 'help'])
+############### create function which will do something by command /start -------> append data to db
+@bot.message_handler(commands=['start'])
 def start(message):
     connect = sqlite3.connect('purple.db')
     cursor = connect.cursor()
@@ -19,33 +15,35 @@ def start(message):
     )""")
     connect.commit()
 
-
-    #check data (uniq users should be ...)
+    ############### check data (uniq users should be ...)
     purple_id = message.chat.id
     cursor.execute(f"select id from purple_data where id = {purple_id}")
     data = cursor.fetchone()
     if data is None:
-        #add values
+        ############### add values
         purple_list = [message.chat.id]
         cursor.execute("INSERT INTO purple_data VALUES(?);", purple_list)
         connect.commit()
     else:
-        bot.send_message(message.chat.id, 'Ooh.... you are already here!')
-    # bot.reply_to(message, "Ooh, howdy.... thanks for join us!")
+        bot.reply_to(message, 'Ooh.... you are already here!')
 
+############### create function which will do something by command /delete -------> delete data from  db
+@bot.message_handler(commands=['delete'])
+def delete(message):
+    connect = sqlite3.connect('purple.db')
+    cursor = connect.cursor()
 
-
+    ##### delete data from bd
+    purple_id = message.chat.id
+    cursor.execute(f'DELETE FROM purple_data WHERE id = {purple_id}')
+    connect.commit()
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
     if message.text == 'эй':
-        bot.reply_to(message, 'В жёпу себе ЭЙ`кни!')
+        bot.reply_to(message, 'Cебе ЭЙ`кни!')
     else:
         bot.reply_to(message, 'I DONT understand you...')
-
-
-    # bot.reply_to(message, message.text)
-
 
 
 bot.infinity_polling()
