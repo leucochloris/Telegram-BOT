6248b967a47904c811736c08d5a61d0e3e1e3175
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-08-22 20:26:37 +0300

    Continue create scennary of conversation beetween user and bot

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import telebot
 import sqlite3
+from aiogram.dispatcher import Dispatcher
+from aiogram import Bot, types
+from aiogram.utils import executor
 
 bot = telebot.TeleBot("5596281432:AAGebrPobQ-RZHe7DnZUFhHRyC6t0rkK198",
                       parse_mode=None)
 
+
+############### greeting output
+@bot.message_handler(commands=['start', 'help'])
+def send_help(message):
+    bot.send_message(message.chat.id,
+                     'Greeting!   I`m bot-assistant, your guide to the world of English language!\n\nAbout us - /about\nRegistration - /reg\nHelp - /help')
+
+
+############### about us output  (who we are...)
+@bot.message_handler(commands=['about'])
+def send_help(message):
+    bot.reply_to(message,
+                 'We are - school of education English language "EASY WAY" 📚📖📕\n\nOur company is young startup with a potential of billion of dollars! 🤑💵💰\n'
+                 '\nWe have schools and representative offices in 5 cities. Other 10 cities are in the plans ✈✈✈\n\n'
+                 'Have any question?! Just write to me: https://vk.com/funnymanalex')
+
+
 ############### create function which will do something by command /start -------> append data to db
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['reg'])
 def start(message):
     connect = sqlite3.connect('purple.db')
     cursor = connect.cursor()
@@ -14,6 +34,7 @@ def start(message):
         id INTEGER
     )""")
     connect.commit()
+    bot.reply_to(message, 'Registration was successful !!!')
 
     ############### check data (uniq users should be ...)
     purple_id = message.chat.id
@@ -27,6 +48,7 @@ def start(message):
     else:
         bot.reply_to(message, 'Ooh.... you are already here!')
 
+
 ############### create function which will do something by command /delete -------> delete data from  db
 @bot.message_handler(commands=['delete'])
 def delete(message):
@@ -38,12 +60,13 @@ def delete(message):
     cursor.execute(f'DELETE FROM purple_data WHERE id = {purple_id}')
     connect.commit()
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    if message.text == 'эй':
+
+@bot.message_handler()
+def angry_user(message):
+    if message.text.lower() == 'эй':
         bot.reply_to(message, 'Cебе ЭЙ`кни!')
-    else:
-        bot.reply_to(message, 'I DONT understand you...')
+    # else:
+    #     bot.reply_to(message, 'I DONT understand you...')
 
 
 bot.infinity_polling()
